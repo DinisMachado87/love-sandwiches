389d2d961a25f6d6906d8c5727335f3af7013e0c
Dinis Machado <137174511+DinisMachado87@users.noreply.github.com>
2023-11-06 12:19:40 +0100

    Add calculated surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -71,11 +71,21 @@ def calculate_surplus_data(sales_row):
     print("calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[len(stock)-1]
+    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
     return surplus_data
+
+def update_surplus_worksheet(surplus_data):
+    """
+    Updates surplus worksheet, add new row with the list data prvided.
+    """
+    print("Updating surplus worksheet... \n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(surplus_data)
+    print("surplus worksheet updates successfuly.\n")
 
 def main():
     """
@@ -85,7 +95,6 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
-
+    update_surplus_worksheet(new_surplus_data)
 print("Welcome to Love Sandwishes Data Automation")
 main()
